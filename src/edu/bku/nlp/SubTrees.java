62af4c0382f91cec5f9d30cb74551d18d57bdc22
Hoang Nguyen <huyhoang8909@gmail.com>
2017-09-27 07:11:46 +0700

    update

--- src/edu/bku/nlp/SubTrees.java
+++ src/edu/bku/nlp/SubTrees.java
@@ -1,14 +1,18 @@
 package edu.bku.nlp;
 
-import edu.stanford.nlp.ling.CoreAnnotations;
-import edu.stanford.nlp.ling.Label;
-import edu.stanford.nlp.ling.Word;
+import edu.stanford.nlp.ling.*;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.trees.*;
+import edu.stanford.nlp.util.*;
 
-import java.util.ArrayList;
-import java.util.Properties;
+import java.io.*;
+import java.util.*;
+
+import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
+import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations.PredictedClass;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
+
 
 public class SubTrees {
 
@@ -20,10 +24,13 @@ public class SubTrees {
         for (Tree leaf : inputTree.getLeaves()) {
             words.addAll(leaf.yieldWords());
         }
-        Label label = inputTree.label();
+        CoreLabel label = (CoreLabel)inputTree.label();
         String cat = label.value();
         if (cat.equals("ROOT") || (previousWords != null && !previousWords.equals(words))) {
-        System.out.print(label+" ");
+          if((label.containsKey(PredictedClass.class))){
+        	  System.out.print(RNNCoreAnnotations.getPredictedClass(inputTree) + " ");
+          }
+        
         for (Word w : words) {
             System.out.print(w.word()+ " ");
         }
@@ -34,21 +41,39 @@ public class SubTrees {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Properties props = new Properties();
-        props.setProperty("annotators", "tokenize,ssplit,pos,parse");
+        props.setProperty("annotators", "tokenize,ssplit,pos,parse,sentiment");
         props.setProperty("parse.binaryTrees", "true");
         StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-        String text = "Yet the act is still charming here.";
+        //call google api translation
+        //log to db
+        String text = "Looks out this Asus is not very good, buy dell vostro 3450 has more than 0.2 kg look better.";
         Annotation annotation = new Annotation(text);
         pipeline.annotate(annotation);
-        Tree sentenceTree = annotation.get(CoreAnnotations.SentencesAnnotation.class).get(0).get(
-                TreeCoreAnnotations.BinarizedTreeAnnotation.class);
-        System.out.println("Penn tree:");
-        sentenceTree.pennPrint(System.out);
-        System.out.println();
-        printSubTrees(sentenceTree, null, "");
+//        Tree sentenceTree = annotation.get(CoreAnnotations.SentencesAnnotation.class).get(0).get(
+//                TreeCoreAnnotations.BinarizedTreeAnnotation.class);
+//        System.out.println("Penn tree:");
+//        sentenceTree.pennPrint(System.out);
+//        System.out.println();
+//        printSubTrees(sentenceTree, null, "");
         
+        PrintWriter out;
+        if (args.length > 1) {
+          out = new PrintWriter(args[1]);
+        } else {
+          out = new PrintWriter(System.out);
+        }
+        List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
+        if (sentences != null && sentences.size() > 0) {
+          ArrayCoreMap sentence = (ArrayCoreMap) sentences.get(0);
 
+          System.out.println("Sentence's keys: ");
+          System.out.println(sentence.keySet());
+          
+          Tree tree2 = sentence.get(SentimentCoreAnnotations.SentimentAnnotatedTree.class);          
+          printSubTrees(tree2, null, "");
+
+        }
     }
 }
